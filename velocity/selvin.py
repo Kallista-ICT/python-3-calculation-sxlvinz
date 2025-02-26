96d842db00613157f7fc1573a3faf7b7a2a07c08
sxlvinz <sxlvinz@gmail.com>
2025-02-26 01:17:07 +0000

    Submit

---
+++ velocity/selvin.py
@@ -0,0 +1,5 @@
+# comment here
+distance = float(input("Enter the distance (in meters): "))
+time = float(input("Enter the time (in seconds): "))
+velocity = distance / time
+print(f"The velocity is {velocity:.2f} m/s.")
